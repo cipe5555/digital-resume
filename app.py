94e800bdf097cc8f8209eadd45dcd17651a2ee53
cipe5555 <jpchong24@gmail.com>
2024-10-20 00:34:42 +0800

    added details

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import base64
 from pathlib import Path
 
 import streamlit as st
@@ -9,6 +10,8 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "CV.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.jpg"
+linkedin_logo  = current_dir / "assets" / "linkedin-original.svg"
+github_logo  = current_dir / "assets" / "github.svg"
 
 
 # --- GENERAL SETTINGS ---
@@ -16,7 +19,7 @@ PAGE_TITLE = "Digital CV | Chong Jack Pang"
 PAGE_ICON = ":wave:"
 NAME = "Chong Jack Pang"
 DESCRIPTION = """
-AI Fresh Graduted, Data & AI enthusiast.
+AI Fresh Graduate, Data & AI enthusiast.
 """
 EMAIL = "jpchong24@gmail.com"
 SOCIAL_MEDIA = {
@@ -24,10 +27,12 @@ SOCIAL_MEDIA = {
     "GitHub": "https://github.com/cipe5555",
 }
 PROJECTS = {
+    "🏆 Final Year Project - Job Recommendation System with Content-Based Filtering Method": "https://github.com/cipe5555/job-recommender",
     "🏆 Timetable Converter - Converting university timetable to calendar event with data engineering approach": "https://github.com/cipe5555/APU-Timetable-to-Google-Calendar",
     "🏆 1st Runner-up in APU-AWS DeepRacer Competition 2023 - Autonomous car racing with Reinforcement Learning (RL)": "https://www.digitalnewsasia.com/business/apu-aws-deepracer-competition-2023-showcases-future-machine-learning-and-data-science",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "🏆 Automated Scene Description with Image Processing": "https://github.com/cipe5555/imaging-and-special-effect",
+    "🏆 Glove Defect Detection System - Implementing computer vision for quality control": "https://github.com/cipe5555/image-processing-and-pattern-recognition",
+    "🏆 Text Analysis and Twitter Sentiment Analysis - Using NLP for sentiment extraction": "https://github.com/cipe5555/text-analysis-and-sentiment-analysis",
 }
 
 
@@ -48,7 +53,7 @@ with col1:
     st.image(profile_pic, width=230)
 
 with col2:
-    st.title(NAME)
+    st.markdown(f"<h1 style='font-size: 42px;'>{NAME}</h1>", unsafe_allow_html=True)
     st.write(DESCRIPTION)
     st.download_button(
         label=" 📄 Download Resume",
@@ -59,11 +64,29 @@ with col2:
     st.write("📫", EMAIL)
 
 
-# --- SOCIAL LINKS ---
+def render_svg(svg_path: Path, width: int = 20):
+    with open(svg_path, "r") as f:
+        svg = f.read()
+    b64 = base64.b64encode(svg.encode("utf-8")).decode("utf-8")
+    html = f'<img src="data:image/svg+xml;base64,{b64}" width="{width}" style="vertical-align:middle; margin-right:10px;" />'
+    return html
+
+# SOCIAL LINKS with SVG logos and links on the same line
 st.write('\n')
 cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+    if platform == "LinkedIn":
+        with cols[index]:
+            linkedin_svg = render_svg(linkedin_logo)
+            st.markdown(f"{linkedin_svg} [**{platform}**]({link})", unsafe_allow_html=True)
+    elif platform == "GitHub":
+        with cols[index]:
+            github_svg = render_svg(github_logo)
+            st.markdown(f"{github_svg} [**{platform}**]({link})", unsafe_allow_html=True)
+
+
+
+
 
 
 # --- EXPERIENCE & QUALIFICATIONS ---
@@ -71,10 +94,13 @@ st.write('\n')
 st.subheader("Experience & Qulifications")
 st.write(
     """
-- ✔️ 7 Years expereince extracting actionable insights from data
-- ✔️ Strong hands on experience and knowledge in Python and Excel
-- ✔️ Good understanding of statistical principles and their respective applications
+- ✔️ Hands-on experience with machine learning frameworks such as PyTorch and TensorFlow, applied to text sentiment detection, heart failure detection, and diabetes detection
+- ✔️ Experienced in scraping, data extraction, and pipeline development using Python libraries like BeautifulSoup and Selenium
 - ✔️ Excellent team-player and displaying strong sense of initiative on tasks
+- ✔️ Developed skills through project-based learning
+- ✔️ Experienced in ETL (Extract, Transform, Load) processes and data warehousing using Snowflake
+- ✔️ Familiar with workflow orchestration tools like Airflow, Dagster, and Prefect for managing and automating complex data pipelines
+- ✔️ Familiar with project management tool like Jira
 """
 )
 
@@ -84,10 +110,14 @@ st.write('\n')
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decition trees
-- 🗄️ Databases: Postgres, MongoDB, MySQL
+- 👩‍💻 **Programming**: Python, Java, C, C++, SQL, Kotlin, Matlab
+- 🛠️ **Data & AI Frameworks**: PyTorch, TensorFlow, Streamlit, Scikit-learn
+- 📊 **Data Visulization**: PowerBi, MS Excel, Matplotlib, Seaborn
+- 📚 **Modeling**: Logistic Regression, Linear Regression, Decision Trees, Random Forest, k-Nearest Neighbors (k-NN)
+- 🗄️ **Databases**: Postgres, MongoDB, MySQL, BigQuery
+- 🔍 **Data Engineering**: ETL Pipelines, Web Scraping, Data Cleaning
+- 🔄 **ETL Tools**: Snowflake, Airflow, Dagster, Prefect
+- 📅 **Project Management**: Jira
 """
 )
 
@@ -119,3 +149,16 @@ st.subheader("Projects & Accomplishments")
 st.write("---")
 for project, link in PROJECTS.items():
     st.write(f"[{project}]({link})")
+
+
+
+# --- LANGUAGES ---
+st.write('\n')
+st.subheader("Languages")
+st.write(
+    """
+- 🇺🇸 **English**: Proficient
+- 🇲🇾 **Malay**: Proficient
+- 🇨🇳 **Mandarin**: Proficient
+"""
+)
